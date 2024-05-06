fda595162eab1578913de11dcf4104b41c06b3c0
Manuel Herrera <98495072+Herre1@users.noreply.github.com>
2024-05-06 12:15:14 -0500

    feat-prueba
    cambio de fork a main

--- Icesi-viajes/src/main/java/co/edu/icesi/viajes/IcesiViajesApplication.java
+++ Icesi-viajes/src/main/java/co/edu/icesi/viajes/IcesiViajesApplication.java
@@ -22,7 +22,7 @@ public class IcesiViajesApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(IcesiViajesApplication.class, args);
 	}
-
+	//Metodo Run
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Prueba conexión");
