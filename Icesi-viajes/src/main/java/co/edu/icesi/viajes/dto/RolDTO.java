1865c75219327e3777fbea972d782cc4f1cf2522
Juan Diego Lora <juan.lora1@u.icesi.edu.co>
2024-06-12 06:25:46 -0500

    rol dto

---
+++ Icesi-viajes/src/main/java/co/edu/icesi/viajes/dto/RolDTO.java
@@ -0,0 +1,40 @@
+package co.edu.icesi.viajes.dto;
+
+import java.util.List;
+
+import co.edu.icesi.viajes.domain.Usuario;
+
+public class RolDTO {
+
+    private Long id;
+    private String nombre;
+    
+	public RolDTO(Long id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+    
+    
+}
